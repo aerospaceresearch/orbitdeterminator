e364ca440eb8d5c2a048b5da30672c3a9fdb0424
nilesh4145 <n.chaturvedi3@gmail.com>
2017-05-21 23:11:17 +0530

    Added code to read data

---
+++ testcases/read_data.py
@@ -0,0 +1,23 @@
+'''
+Author : Nilesh Chaturvedi
+Date Created : 21/05/2017
+'''
+
+import os
+import sys
+import csv
+import numpy as np
+
+def load_data(filename):
+	'''
+	Returns a list of numpy arrays with each array of the format 
+	[timestamp, x-coordinate, y-coordinate, z-coordinate]
+	'''
+
+	data_list = list(csv.reader(open(filename, "r"), delimiter = ","))
+	orbit = []
+	for i in data_list:
+		data_tup = np.array(i, dtype = np.float)
+		orbit.append(data_tup)
+
+	return orbit
