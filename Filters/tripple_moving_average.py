040d10d95c07c8e16da7d4c94dd23364a714c326
nilesh4145 <n.chaturvedi3@gmail.com>
2017-06-01 03:09:55 +0530

    Code for weighted average

--- Filters/tripple_moving_average.py
+++ Filters/tripple_moving_average.py
@@ -8,19 +8,41 @@ A = weighted average of n terms previous to x0
 B = weighted avreage of n terms ahead of x0
 n = window size
 '''
+def weighted_average(params):
+    '''
+    Calculates the weighted average of terms in the input
+
+    Args:
+        params: a list of numbers
+
+    Returns:
+        weighted average of the terms in the list
+    '''
+    weighted_sum = 0
+    weight = len(params)
+    weight_sum = weight * (weight+1) / 2
+    
+    for num in params:
+        weighted_sum += weight*num
+        weight -= 1
+
+    return weighted_sum / weight_sum
+
 
 def triple_moving_average(signal_array, window_size):
     '''
     Apply triple moving average to a signal
 
     Args:
-        signal_array : The array of values on which the filter is to be applied
-        window_size : The no. of points before and after x0 which should be 
+        signal_array: the array of values on which the filter is to be applied
+        window_size: the no. of points before and after x0 which should be 
         considered for calculating A and B
 
     Returns:
        A filtered array of size same as that of signal_array 
     '''
+
+
     pass
 
 
@@ -30,4 +52,4 @@ if __name__ = "__main__":
 
     moving_window = 3
 
-    averaged_signal = triple_moving_average(signal_x, moving_w)
+    averaged_signal = triple_moving_average(signal_x, moving_window)
