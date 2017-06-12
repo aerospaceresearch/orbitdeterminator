a7b62ce4c71f4b8244d01db5794e6c5053dfd382
Alexandros23Kazantzidis <alexandroskaza23@gmail.com>
2017-06-12 16:53:57 +0300

    Create example_init.py

--- testcases/Alexandros code/example_init.py
+++ testcases/Alexandros code/example_init.py
@@ -66,16 +66,16 @@ positions2 = keep_state2[0:3, :]
 df = pd.DataFrame(positions)
 
 
-# mpl.rcParams['legend.fontsize'] = 10
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-#
-# ax.plot(my_data[:, 1], my_data[:, 2], my_data[:, 3], "o", label='Filtered data with golay')
-# ax.plot(positions[0, :], positions[1, :], positions[2, :], "r-", label='Orbit after lamberts - kalman')
-# ax.plot(positions2[0, :], positions2[1, :], positions2[2, :], "o-", label='Perfect orbit')
-# ax.legend()
-# ax.can_zoom()
-# ax.set_xlabel('x (km)')
-# ax.set_ylabel('y (km)')
-# ax.set_zlabel('z (km)')
-# plt.show()
+mpl.rcParams['legend.fontsize'] = 10
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot(my_data[:, 1], my_data[:, 2], my_data[:, 3], "o", label='Filtered data with golay')
+ax.plot(positions[0, :], positions[1, :], positions[2, :], "r-", label='Orbit after lamberts - kalman')
+ax.plot(positions2[0, :], positions2[1, :], positions2[2, :], "o-", label='Perfect orbit')
+ax.legend()
+ax.can_zoom()
+ax.set_xlabel('x (km)')
+ax.set_ylabel('y (km)')
+ax.set_zlabel('z (km)')
+plt.show()
