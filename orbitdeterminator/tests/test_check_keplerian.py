d9a3a81762eb64e60c07e6a454a5f9c91157773d
Alexandros23Kazantzidis <alexandroskaza23@gmail.com>
2017-07-12 22:23:31 +0300

    small changes into testing

--- orbitdeterminator/tests/test_check_keplerian.py
+++ orbitdeterminator/tests/test_check_keplerian.py
@@ -18,17 +18,18 @@ kep2 = np.array([[20000, 0.3, 90.0, 0.8, 0.0, 28],
     (kep1, kep1),
     (kep2, kep2),
 ])
-def test_simple_pass(given, expected):
+def test_check_keplerian(given, expected):
     assert_array_equal(lamberts_kalman.check_keplerian(given), expected)
 
+
+# Second test checks the function with three rows that the first and second row has wrong inputs
 kep1 = np.array([[15700, -0.3, 90.0, 0.8, 0.0, 28],
                 [-13700, 0.4, 90.0, 0.8, 0.2, 28],
                 [13700, 0.4, 90.0, 0.8, 0.2, 28.]])
 
 
-# Second test checks the function with three rows that the first and second row has wrong inputs
 @pytest.mark.xfail(reason="First row has negative eccentricity, Second row has negative semi major axis")
-def test_fail(given, expected):
+def test_check_keplerian_fail():
     assert_array_equal(lamberts_kalman.check_keplerian(kep1), kep1)
 
 
@@ -38,7 +39,7 @@ kep2 = np.array([[20000, 0.3, 90.0, 0.8, 0.0, 28.0],
 
 
 @pytest.mark.xfail(reason=" Second row has eccentricity > 1")
-def test_fail_2():
+def test_check_keplerian_fail_2():
     assert_array_equal(lamberts_kalman.check_keplerian(kep2), kep2)
 
 
