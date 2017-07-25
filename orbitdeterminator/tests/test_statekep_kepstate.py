c19a2ccb6fc87b0ad281e434500e09e8fc59e08a
Alexandros23Kazantzidis <alexandroskaza23@gmail.com>
2017-07-25 13:13:41 +0300

    State (#65)
    * fixed an error in state_kep.py
    
    * pytesting adjustments

--- orbitdeterminator/tests/test_statekep_kepstate.py
+++ orbitdeterminator/tests/test_statekep_kepstate.py
@@ -8,13 +8,13 @@ import numpy.testing as npt
 
 # Test the vice versa procedure with an uncertainty of 10 decimal points
 def test_statekep_kepstate():
-	# Initial state vector which is going to be transformed into keplerian elements and then back to the initial state
-	kep = np.array([[15711.578566], [0.377617], [90.0], [0.887383], [0.0], [28.357744]])
-	y = kep_state.kep_state(kep)
+    # Initial state vector which is going to be transformed into keplerian elements and then back to the initial state
+    kep = np.array([[15711.578566], [0.377617], [90.0], [0.887383], [0.0], [28.357744]])
+    y = kep_state.kep_state(kep)
 
-	expected = np.ravel(kep)
-	r_given = np.array([y[0, 0], y[1, 0], y[2, 0]])
-	v_given = np.array([y[3, 0], y[4, 0], y[5, 0]])
+    expected = np.ravel(kep)
+    r_given = np.array([y[0, 0], y[1, 0], y[2, 0]])
+    v_given = np.array([y[3, 0], y[4, 0], y[5, 0]])
 
-	npt.assert_almost_equal(state_kep.state_kep(r_given, v_given), expected, decimal=10)
+    npt.assert_almost_equal(state_kep.state_kep(r_given, v_given), expected, decimal=10)
 
