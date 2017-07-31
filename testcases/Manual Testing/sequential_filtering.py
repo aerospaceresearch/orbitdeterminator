72699356c3e78bad915bc6c9c5c2f265107d51b8
nilesh4145 <n.chaturvedi3@gmail.com>
2017-07-31 20:43:01 +0530

    Parallelized testing code

--- testcases/Manual Testing/sequential_filtering.py
+++ testcases/Manual Testing/sequential_filtering.py
@@ -12,28 +12,30 @@ import read_data as rd
 import tripple_moving_average as tma
 import interpolation
 import pickle
+from multiprocessing import Pool, Manager
+manager = Manager()
+final_dict = manager.dict()
 
+def process(file):
+    signal = rd.load_data(os.getcwd() + "/track/" + file)
+    for tma_window in range(2, 9):
+        print(tma_window)
+        tma_filtered = tma.generate_filtered_data(signal, tma_window)
+        for golay_degree in range(1, 9):
+            print (golay_degree)
+            for golay_window in range(9, 99, 2):
+                print(golay_window)
+                print(file)
+                output = open("result.txt", 'a')
+                golay_filtered = golay(tma_filtered, golay_window, golay_degree)
+                param_state = tuple([file, tma_window, golay_window, golay_degree])
+                a = file[16:30]
+                kep = interpolation.main(golay_filtered)  # Keplerian determination happens here. Change this accordingly
+                error = abs(kep_dict[a] - kep).mean(axis=0)
+                output.write(str(param_state) + " : " + str(error))
+                output.close()
 
-# def process(file):
-#     signal = rd.load_data(os.getcwd() + "/track/" + file)
-#     for tma_window in range(2, 9):
-#         print(tma_window)
-#         tma_filtered = tma.generate_filtered_data(signal, tma_window)
-#         for golay_degree in range(1, 9):
-#             print (golay_degree)
-#             for golay_window in range(9, 99, 2):
-#                 print(golay_window)
-#                 print(file)
-#                 output = open("result.txt", 'a')
-#                 golay_filtered = golay(tma_filtered, golay_window, golay_degree)
-#                 param_state = tuple([file, tma_window, golay_window, golay_degree])
-#                 a = file[16:30]
-#                 kep = interpolation.main(golay_filtered)  # Keplerian determination happens here. Change this accordingly
-#                 error = abs(kep_dict[a] - kep).mean(axis=0)
-#                 output.write(str(param_state) + " : " + str(error))
-#                 output.close()
-
-#                 final_dict[param_state] = error
+                final_dict[param_state] = error
 
 def golay(data, window, degree):
 
@@ -58,7 +60,7 @@ noisy_files = os.listdir(noisy_path)
 true_keps = open("true_kep_dict.p", 'rb')
 kep_dict = pickle.load(true_keps)
 print(kep_dict)
-final_dict = {}
+#final_dict = {}
 ######################################################################################
 # noise_0, noise_10000, noise_20000, noise_40000, noise_80000 = [], [], [], [], []
 # [noise_0.append(i) for i in noisy_files if i.endswith("0.csv")]
@@ -68,26 +70,28 @@ final_dict = {}
 # [noise_80000.append(i) for i in noisy_files if i.endswith("80000.csv")]
 ######################################################################################
 
-for file in noisy_files:
-    signal = rd.load_data(os.getcwd() + "/track1/" + file)
-    for tma_window in range(2, 9):
-        print(tma_window)
-        tma_filtered = tma.generate_filtered_data(signal, tma_window)
-        for golay_degree in range(1, 9):
-            print (golay_degree)
-            for golay_window in range(9, 99, 2):
-                print(golay_window)
-                print(file)
-                output = open("result.txt", 'a')
-                golay_filtered = golay(tma_filtered, golay_window, golay_degree)
-                param_state = tuple([file, tma_window, golay_window, golay_degree])
-                a = file[16:30]
-                kep = interpolation.main(golay_filtered)  # Keplerian determination happens here. Change this accordingly
-                error = abs(kep_dict[a] - kep).mean(axis=0)
-                output.write(str(param_state) + " : " + str(error))
-                output.close()
+pool = Pool()
+pool.map(process, noisy_files)
+# for file in noisy_files:
+    # signal = rd.load_data(os.getcwd() + "/track1/" + file)
+    # for tma_window in range(2, 9):
+    #     print(tma_window)
+    #     tma_filtered = tma.generate_filtered_data(signal, tma_window)
+    #     for golay_degree in range(1, 9):
+    #         print (golay_degree)
+    #         for golay_window in range(9, 99, 2):
+    #             print(golay_window)
+    #             print(file)
+    #             output = open("result.txt", 'a')
+    #             golay_filtered = golay(tma_filtered, golay_window, golay_degree)
+    #             param_state = tuple([file, tma_window, golay_window, golay_degree])
+    #             a = file[16:30]
+    #             kep = interpolation.main(golay_filtered)  # Keplerian determination happens here. Change this accordingly
+    #             error = abs(kep_dict[a] - kep).mean(axis=0)
+    #             output.write(str(param_state) + " : " + str(error))
+    #             output.close()
 
-                final_dict[param_state] = error
+    #             final_dict[param_state] = error
 
 output.close()
 pickle.dump(final_dict, open("final_dict.p", 'wb'))
