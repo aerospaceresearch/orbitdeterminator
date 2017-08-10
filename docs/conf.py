8ce022659f200fe95cc843e406643d604e13fbd3
Nilesh Chaturvedi <Nilesh4145@users.noreply.github.com>
2017-08-10 16:50:13 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = '_static/icon.png'
+html_logo = 'icon.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
