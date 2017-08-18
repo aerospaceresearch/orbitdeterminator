fe1834850d1540d432fba360b1ae06cb6c9bbb86
Alexandros23Kazantzidis <alexandroskaza23@gmail.com>
2017-08-18 12:24:39 +0300

    adding new setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
-##Setup file for orbitdeterminator
+from setuptools import *
+
+setup(
+	name="orbitdeterminator",
+	packages=find_packages(),
+	version="1.0",
+	license="MIT",
+	author="Alexandros Kazantzidis, Nilesh Chaturvedi",
+	author_email="alexandroskaza23@gmail.com, n.chaturvedi3@gmail.com",
+	url="http://orbit-determinator.readthedocs.io/en/latest/",
+	description="__orbitdeterminator__ is a package written in python3 for determining orbit of satellites based " \
+				  "on positional data. Various filtering and determination algorithms are available for satellite " \
+				  "operators to choose from.  ",
+	install_requires=[
+		"numpy",
+		"scipy",
+		"matplotlib",
+		"pykep",
+		"pytest",
+	]
+)
