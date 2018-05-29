0b5bbb0c9bf0cb28f2c30ffb6c9e5e2872af43c8
Jorge Pérez <jperez@icf.unam.mx>
2018-05-29 12:51:10 -0500

    Add function to read IOD-formatted files

--- orbitdeterminator/util/read_data.py
+++ orbitdeterminator/util/read_data.py
@@ -41,6 +41,29 @@ def save_orbits(source, destination):
             orbit = load_data(source + '/' + str(file))
             pickle.dump(orbit, open(destination + "/%s.p" %file[:-4], "wb"))
 
+def load_data_iod_af2(fname):
+    '''
+    Loads satellite position observation data from IOD-formatted files.
+    Currently, the only supported angle format is 2, as specified in IOD format.
+    TODO: add other angle formats.
+
+    Args:
+        fname (string): name of the IOD-formatted text file to be parsed
+
+    Returns:
+        x (numpy array): array of satellite position observations following the
+        IOD format, with angle format code = 2.
+    '''
+    # dt is the dtype for IOD-formatted text files
+    dt = 'S15,i8,S1,i8,i8,i8,i8,i8,i8,S1,i8,i8,S1'
+    # iod_names correspond to the dtype names of each field
+    iod_names = ['object','station','stationstatus','utcdate','utctime','timeunc','angformat','epoch','ra','decsgn','dec','posunc','optical','vismag']
+    # TODO: read first line, get sub-format, construct iod_delims from there
+    # as it is now, it only works for the given test file iod_data.txt
+    # iod_delims are the fixed-width column delimiter followinf IOD format description
+    iod_delims = [15,5,2,9,9,3,2,1,8,1,6,3,2,2]
+    return np.genfromtxt(fname, dtype=dt, names=iod_names, delimiter=iod_delims, autostrip=True)
+
 if __name__ == "__main__":
 
     save_orbits(_SOURCE, _DESTINATION)
