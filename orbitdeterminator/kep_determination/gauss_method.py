fa88d33fb649b8fba7c53a3274722f773efa7931
Jorge Pérez <jperez@icf.unam.mx>
2018-06-19 20:10:16 -0500

    Fix gmst; add comments and examples

--- orbitdeterminator/kep_determination/gauss_method.py
+++ orbitdeterminator/kep_determination/gauss_method.py
@@ -16,13 +16,22 @@ import numpy as np
 
 # compute Greenwich mean sidereal time (in hours) at UT instant of Julian date JD0:
 def gmst(jd0, ut):
-    return 6.656306 + 0.0657098242*(jd0-2445700.5) + 1.0027379093*ut
+    return np.mod(6.656306 + 0.0657098242*(jd0-2445700.5) + 1.0027379093*ut, 24.0)
+
+# compute Greenwich apparent sidereal time (in hours) at UT instant of Julian date JD0:
+# delta_lambda: nutation in longitude (hours)
+# epsilon: obliquity of the ecliptic (degrees)
+def gast(jd0, ut, delta_lambda, epsilon):
+    gmst_hrs = gmst(jd0, ut)
+    return np.mod(gmst_hrs+delta_lambda*np.cos(epsilon), 24.0)
 
 # compute local sidereal time from GMST and longitude EAST of Greenwich:
 def localsidtime(gmst_hrs, long):
-    return np.mod((gmst_hrs+long*(1.0/15.0)),24.0)
+    return np.mod((gmst_hrs+long/15.0),24.0)
 
-#top of page 266, chapter 5, Orbital Mechanics book
+# geocentric observer position at a given longitude,
+# parallax constants S and C, Julian date jd0 and UT time ut
+# formula taken from top of page 266, chapter 5, Orbital Mechanics book
 def observerpos(long, parallax_s, parallax_c, jd0, ut):
 
     # compute geocentric latitude from parallax constants S and C
@@ -34,10 +43,86 @@ def observerpos(long, parallax_s, parallax_c, jd0, ut):
     # Earth's mean radius in kilometers
     Re = 6378.0
 
+    # compute cartesian components of geocentric observer position
     x_gc = Re*np.cos(phi_gc)*np.cos(15.0*lst_hrs)
     y_gc = Re*np.cos(phi_gc)*np.sin(15.0*lst_hrs)
     z_gc = Re*np.sin(phi_gc)
 
     return np.array((x_gc,y_gc,z_gc))
 
+#########################
+
+# an example of computation of sidereal times, which will be added as unit testing:
+# Determine the true and mean sidereal time on 01 January 1982 at 1h CET for
+# Munich (lambda = -11deg36.5', delta_lambda = -15''.476, epsilon = 23deg26'27'')
+
+jd0_ = 2444970.5 #Julian date of 1982, January 1st
+ut = 0.0
+munich_long = (11.0+36.5/60.0) #degrees
+my_d_lamb = -(15.476)/15.0/3600.0 # hours
+epsilon_ = np.deg2rad( 23.0+26.0/60.0+27.0/3600.0 ) #radians
+
+print('JD = ', 2444970.5)
+print('dl*cos(e) = ', my_d_lamb*np.cos(epsilon_), 's')
+
+mu_gmst = gmst(jd0_,ut)
+mu_gmst_hrs = np.floor(mu_gmst)
+mu_gmst_min = np.floor((mu_gmst-mu_gmst_hrs)*60.0)
+mu_gmst_sec = ((mu_gmst-mu_gmst_hrs)*60.0-mu_gmst_min)*60.0
+
+mu_gast = gast(jd0_, ut, my_d_lamb, epsilon_)
+mu_gast_hrs = np.floor(mu_gast)
+mu_gast_min = np.floor((mu_gast-mu_gast_hrs)*60.0)
+mu_gast_sec = ((mu_gast-mu_gast_hrs)*60.0-mu_gast_min)*60.0
+
+mu_lmst = localsidtime(mu_gmst, munich_long)
+mu_lmst_hrs = np.floor(mu_lmst)
+mu_lmst_min = np.floor((mu_lmst-mu_lmst_hrs)*60.0)
+mu_lmst_sec = ((mu_lmst-mu_lmst_hrs)*60.0-mu_lmst_min)*60.0
+
+mu_last = localsidtime(mu_gast, munich_long)
+mu_last_hrs = np.floor(mu_last)
+mu_last_min = np.floor((mu_last-mu_last_hrs)*60.0)
+mu_last_sec = ((mu_last-mu_last_hrs)*60.0-mu_last_min)*60.0
+
+print('GMST = ', mu_gmst )
+print('mu_gmst_hrs = ', mu_gmst_hrs)
+print('mu_gmst_min = ', mu_gmst_min)
+print('mu_gmst_sec = ', mu_gmst_sec)
+
+print('GAST = ', mu_gast )
+print('mu_gast_hrs = ', mu_gast_hrs)
+print('mu_gast_min = ', mu_gast_min)
+print('mu_gast_sec = ', mu_gast_sec)
+
+print('LMST = ', mu_lmst )
+print('mu_lmst_hrs = ', mu_lmst_hrs)
+print('mu_lmst_min = ', mu_lmst_min)
+print('mu_lmst_sec = ', mu_lmst_sec)
+
+print('LAST = ', mu_last )
+print('mu_last_hrs = ', mu_last_hrs)
+print('mu_last_min = ', mu_last_min)
+print('mu_last_sec = ', mu_last_sec)
+
+# Julian date of Apophis discovery observations:
+jd = 2453079.5 # 2004 Mar 15
+ut = 24.0*0.10789 # UT time of 1st observation
+
+# longitude and parallax constants C,S for observatory with code 691:
+# 248.4010  0.84951  +0.52642
+# taken from https://www.minorplanetcenter.net/iau/lists/ObsCodesF.html
+# retrieved on: 19 Jun 2018
+
+long_691 = 248.4010 # degrees
+C_691 = 0.84951
+S_691 = +0.52642
+
+#geocentric observer position at time of 1st Apophis observation:
+pos_691 = observerpos(long_691, S_691, C_691, jd, ut)
+print('pos_691 = ', pos_691)
+
+# cross-check:
+radius_ = np.sqrt(pos_691[0]**2+pos_691[1]**2+pos_691[2]**2)
+print('radius_ = ', radius_)
 
