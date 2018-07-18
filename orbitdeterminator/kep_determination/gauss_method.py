7f6d5fef2c7c6e3baad4d1c9be56795db9968243
Jorge Pérez <jperez@icf.unam.mx>
2018-07-18 11:03:53 -0500

    Cleanup: move Ceres example to separate file

--- orbitdeterminator/kep_determination/gauss_method.py
+++ orbitdeterminator/kep_determination/gauss_method.py
@@ -13,6 +13,8 @@ from astropy.time import Time
 from datetime import datetime
 # from poliastro.stumpff import c2, c3
 
+np.set_printoptions(precision=16)
+
 def load_mpc_observatories_data(mpc_observatories_fname):
     obs_dt = 'S3, f8, f8, f8, S48'
     obs_delims = [4,10,9,10,48]
@@ -313,7 +315,7 @@ def gauss_polynomial(x, a, b, c):
 # Implementation of Gauss method for MPC optical observations of NEAs
 def gauss_estimate_mpc(mpc_observatories_data, inds, mpc_data_fname, r2_root_ind=0):
     # load JPL DE430 ephemeris SPK kernel, including TT-TDB difference
-    kernel = SPK.open('de430.bsp')
+    kernel = SPK.open('de430t.bsp')
 
     # print(kernel)
 
@@ -908,9 +910,7 @@ def gauss_method_mpc(mpc_observatories_data, inds_, mpc_data_fname, refiters=0, 
     return r1, r2, r3, v2, R, rho1, rho2, rho3, rho_1_, rho_2_, rho_3_, Ea_hc_pos
 
 ##############################
-if __name__ == "__main__":
-
-    np.set_printoptions(precision=16)
+#if __name__ == "__main__":
 
     # # Examples 5.11 and 5.12 from book
     # phi_deg = 40.0 # deg
@@ -962,140 +962,6 @@ if __name__ == "__main__":
     #     x_orb_vec[i] = recovered_xyz[0]
     #     y_orb_vec[i] = recovered_xyz[1]
     #     z_orb_vec[i] = recovered_xyz[2]
-
-    # ##############################
-
-    au = 1.495978707e8
-
-    # obs_arr = list(range(0,4))+list(range(7,88))+list(range(93,310))+list(range(335,976))+list(range(985,1102))+list(range(1252,1260))
-    # obs_arr = list(range(0,4))+list(range(7,110))
-    # obs_arr = [1114, 1136, 1251]
-    
-    # obs_arr = list(range(986,1249))
-    # obs_arr = list(range(986,990))
-    # obs_arr = list(range(0,4))+list(range(7,11))
-    # obs_arr = list(range(335,340))
-    # obs_arr = list(range(7,15))
-    
-    # obs_arr = list(range(860,978))
-    
-    # obs_arr = list(range(0,20))
-    # obs_arr = [0, 10, 20] # Ceres
-    # obs_arr = [7214,7233,7256]
-    # obs_arr = list(range(7214,7256))
-    obs_arr = [7145,7146,7148,7152,7155,7156,7157,7158,7159,7164,7172,7178,7185,7190,7197,7201,7205,7213,7214,7218,7219,7221,7222,7227,7231,7240,7241,7242,7250]
-    # obs_arr = [7106,7117,7133]
-    # ,7133,7145
-    # obs_arr = [7106,7117,7146,7148,7152,7155,7156,7157,7158,7159,7164,7172,7178,7185,7190,7197,7201,7205,7213,7214,7218,7219,7221,7222,7227,7231,7240,7241,7242,7250]
-    nobs = len(obs_arr)
-    print('nobs = ', nobs)
-    print('obs_arr = ', obs_arr)
-
-    x_vec = np.zeros((nobs-2,))
-    y_vec = np.zeros((nobs-2,))
-    z_vec = np.zeros((nobs-2,))
-
-    x_Ea_vec = np.zeros((nobs-2,))
-    y_Ea_vec = np.zeros((nobs-2,))
-    z_Ea_vec = np.zeros((nobs-2,))
-
-    a_vec = np.zeros((nobs-2,))
-    e_vec = np.zeros((nobs-2,))
-    I_vec = np.zeros((nobs-2,))
-    W_vec = np.zeros((nobs-2,))
-    w_vec = np.zeros((nobs-2,))
-
-    r2_root_ind_vec = np.zeros((nobs-2,), dtype=int)
-
-    print('r2_root_ind_vec = ', r2_root_ind_vec)
-    # print('np.isnan(r2s_guess_vec[7]) = ', np.isnan(r2s_guess_vec[7]))
-
-    mpc_observatories_data = load_mpc_observatories_data('mpc_observatories.txt')
-
-    mu_Sun = 0.295912208285591100E-03 # Sun's G*m, au^3/day^2
-    mu = mu_Sun
-
-    ###########################
-    print('len(range (0,nobs-2)) = ', len(range (0,nobs-2)))
-    for j in range (0,nobs-2):
-        # Apply Gauss method to three elements of data
-        inds_ = [obs_arr[j]-1, obs_arr[j+1]-1, obs_arr[j+2]-1]
-        print('j = ', j)
-        # r1, r2, r3, v2, R, rho1, rho2, rho3, rho_1_, rho_2_, rho_3_, Ea_hc_pos = gauss_method_mpc(mpc_observatories_data, inds_, '../example_data/mpc_apophis_data.txt', refiters=5)
-        r1, r2, r3, v2, R, rho1, rho2, rho3, rho_1_, rho_2_, rho_3_, Ea_hc_pos = gauss_method_mpc(mpc_observatories_data, inds_, '../example_data/mpc_ceres_data.txt', refiters=5, r2_root_ind=r2_root_ind_vec[j])
-
-        # print('|r1| = ', np.linalg.norm(r1,ord=2))
-        # print('|r2| = ', np.linalg.norm(r2,ord=2))
-        # print('|r3| = ', np.linalg.norm(r3,ord=2))
-        # print('r2 = ', r2)
-        # print('v2 = ', v2)
-
-        # print("*** CARTESIAN STATES AND REFERENCE EPOCH ***")
-
-        # print('r2 = ', r2/au, 'au')
-        # print('v2 = ', v2, 'au/day')
-        # print('JD2 = ', jd2, '\n')
-
-        a_num = semimajoraxis(r2[0], r2[1], r2[2], v2[0], v2[1], v2[2], mu)
-        e_num = eccentricity(r2[0], r2[1], r2[2], v2[0], v2[1], v2[2], mu)
-
-        #    if 0.0<=e_num<=1.0 and a_num>=0.0:
-        a_vec[j] = a_num
-        e_vec[j] = e_num
-        I_vec[j] = np.rad2deg( inclination(r2[0], r2[1], r2[2], v2[0], v2[1], v2[2]) )
-        W_vec[j] = np.rad2deg( longascnode(r2[0], r2[1], r2[2], v2[0], v2[1], v2[2]) )
-        w_vec[j] = np.rad2deg( argperi(r2[0], r2[1], r2[2], v2[0], v2[1], v2[2], mu) )
-        # print(a_vec[j], e_vec[j], I_vec[j], W_vec[j], w_vec[j])
-        x_vec[j] = r2[0]
-        y_vec[j] = r2[1]
-        z_vec[j] = r2[2]
-        x_Ea_vec[j] = Ea_hc_pos[1][0]
-        y_Ea_vec[j] = Ea_hc_pos[1][1]
-        z_Ea_vec[j] = Ea_hc_pos[1][2]
-
-        # print(a_num/au, 'au', ', ', e_num)
-        # print(a_num, 'au', ', ', e_num)
-        # print('j = ', j, 'obs_arr[j] = ', obs_arr[j])
-
-    # print('x_vec = ', x_vec)
-    # print('a_vec = ', a_vec)
-    # print('e_vec = ', e_vec)
-    print('a_vec = ', a_vec)
-    print('len(a_vec) = ', len(a_vec))
-    print('len(a_vec[a_vec>0.0]) = ', len(a_vec[a_vec>0.0]))
-
-    print('e_vec = ', e_vec)
-    print('len(e_vec) = ', len(e_vec))
-    e_vec_fil1 = e_vec[e_vec<1.0]
-    e_vec_fil2 = e_vec_fil1[e_vec_fil1>0.0]
-    print('len(e_vec[e_vec<1.0]) = ', len(e_vec_fil2))
-
-    print('*** AVERAGE ORBITAL ELEMENTS: a, e, I, Omega, omega ***')
-    # print('Semimajor axis, a: ', a_, 'km')
-    # print(np.mean(a_vec[a_vec>0.0])/au, 'au', ', ', np.mean(e_vec[e_vec<1.0]))
-    print(np.mean(a_vec), 'au', ', ', np.mean(e_vec), ', ', np.mean(I_vec), 'deg', ', ', np.mean(W_vec), 'deg', ', ', np.mean(w_vec), 'deg')
-
-    ###########################
-    # Plot
-
-    from mpl_toolkits import mplot3d
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    # Sun-centered orbits: Computed orbit and Earth's
-    ax.scatter3D(x_vec[x_vec!=0.0], y_vec[x_vec!=0.0], z_vec[x_vec!=0.0], color='red', marker='.', label='Computed orbit')
-    ax.scatter3D(x_Ea_vec[x_Ea_vec!=0.0], y_Ea_vec[x_Ea_vec!=0.0], z_Ea_vec[x_Ea_vec!=0.0], color='blue', marker='.', label='Earth orbit')
-    ax.scatter3D(0.0, 0.0, 0.0, color='yellow', label='Sun')
-    plt.legend()
-    ax.set_xlabel('x (au)')
-    ax.set_ylabel('y (au)')
-    ax.set_zlabel('z (au)')
-    ax.set_xlim(-3.5, 3.5)
-    ax.set_ylim(-3.5, 3.5)
-    ax.set_zlim(-3.5, 3.5)
-    plt.title('Angles-only orbit determ. (Gauss)')
-    plt.show()
-    # end, Apophis plot
 
     # xline1 = np.array((0.0, R[0][0]))
     # yline1 = np.array((0.0, R[0][1]))
