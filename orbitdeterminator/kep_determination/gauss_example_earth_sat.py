830cc128925b025ef6e17fabbf4d8f866d00e634
Jorge Pérez <jperez@icf.unam.mx>
2018-07-25 13:06:27 -0500

    Small update

--- orbitdeterminator/kep_determination/gauss_example_earth_sat.py
+++ orbitdeterminator/kep_determination/gauss_example_earth_sat.py
@@ -8,9 +8,7 @@ body_fname_str = '../example_data/iod_data_af2.txt'
 body_name_str = '43145'
 
 #lines of observations file to be used for orbit determination
-obs_arr = [1, 3, 4, 6, 8] #np.array(range(1,4)) #, 5, 6, 7] #LB and PP obs on 2018-07-(08, 09)
-# obs_arr = [8, 9, 10, 11, 12, 13, 14] #LB and PP obs on 2018-07-(23, 24)
-# obs_arr = [12, 13, 14] #LB and PP obs on 2018-07-(23, 24)
+obs_arr = [1, 3, 4, 6, 8] # LB observations of 21799 91 076C on 2018 Jul 22
 
 #the total number of observations used
 nobs = len(obs_arr)
@@ -19,6 +17,8 @@ nobs = len(obs_arr)
 r2_root_ind_vec = np.zeros((nobs-2,), dtype=int)
 # r2_root_ind_vec[0] = 1 # uncomment and modify as required if adequate root of Gauss polynomial has to be selected
 
-# a_mean, e_mean, taup_mean, I_mean, W_mean, w_mean = gm.gauss_method_mpc(body_fname_str, body_name_str, obs_arr, r2_root_ind_vec, refiters=5)
+a, e, taup, I, W, w, T = gm.gauss_method_sat(body_fname_str, body_name_str, obs_arr, r2_root_ind_vec, refiters=10)
 
-gm.gauss_method_sat(body_fname_str, body_name_str, obs_arr, r2_root_ind_vec, refiters=10)
+x = np.array((a, e, taup, I, W, w, T))
+
+print('x = ', x)
