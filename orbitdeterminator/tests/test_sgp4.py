b3b225de63446ed365790d0dab5ca9c3eabe0fb1
Aakash Deep <aakash15001@iiitd.ac.in>
2018-07-31 18:42:29 +0530

    travis check updated

--- orbitdeterminator/tests/test_sgp4.py
+++ orbitdeterminator/tests/test_sgp4.py
@@ -10,9 +10,9 @@ def test_proapagation_model():
 
     line1 = "1 88888U          80275.98708465  .00073094  13844-3  66816-4 0     8"
     line2 = "2 88888  72.8435 115.9689 0086731  52.6988 110.5714 16.05824518   105"
-    obj.compute_necessary(line1, line2)
+    obj.compute_necessary_tle(line1, line2)
     tsince = 0
-    r,v = obj.propagation_model(tsince)
+    r,v = obj.propagation_model(0,tsince)
     r = [float("{0:.5f}".format(i)) for i in r]
     v = [float("{0:.5f}".format(i)) for i in v]
     pos = [2328.97070, -5995.22083, 1719.97066]
@@ -23,9 +23,9 @@ def test_proapagation_model():
 
     line1 = "1 32785U 08021C   18201.86927515  .00000199  00000-0  27157-4 0  9996"
     line2 = "2 32785  97.5464 212.4389 0011563 289.3405  70.6562 14.88147354554182"
-    obj.compute_necessary(line1, line2)
+    obj.compute_necessary_tle(line1, line2)
     tsince = 0
-    r,v = obj.propagation_model(tsince)
+    r,v = obj.propagation_model(0,tsince)
     r = [float("{0:.5f}".format(i)) for i in r]
     v = [float("{0:.5f}".format(i)) for i in v]
     pos = [-5892.75718, -3745.27150, 0.00265]
@@ -36,9 +36,9 @@ def test_proapagation_model():
 
     line1 = "1 27844U 03031E   18209.96155204  .00000018  00000-0  28062-4 0  9995"
     line2 = "2 27844  98.6862 218.0011 0008601 248.4534 111.5728 14.22124843782096"
-    obj.compute_necessary(line1, line2)
+    obj.compute_necessary_tle(line1, line2)
     tsince = 0
-    r,v = obj.propagation_model(tsince)
+    r,v = obj.propagation_model(0,tsince)
     r = [float("{0:.5f}".format(i)) for i in r]
     v = [float("{0:.5f}".format(i)) for i in v]
     pos = [-5674.78436, -4433.80110, 0.00928]
