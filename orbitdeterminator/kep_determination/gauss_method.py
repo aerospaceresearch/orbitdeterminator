Fix elevation units

orbitdeterminator/kep_determination/gauss_method.py
@@ -167,7 +167,7 @@ def observerpos_mpc(long, parallax_s, parallax_c, t_utc):
 # formula taken from bottom of page 265 (Eq. 5.56), chapter 5, Orbital Mechanics book (Curtis)
 # lat: geodetic latitude (deg)
 # long: longitude (deg)
-# elev: elevation above reference ellipsoid (km)
+# elev: elevation above reference ellipsoid (m)
 # t_utc: time (UTC)
 def observerpos_sat(lat, long, elev, t_utc):
 
@@ -196,8 +196,8 @@ def observerpos_sat(lat, long, elev, t_utc):
     cos_phi_sin_theta = cos_phi*np.sin( lmst_rad )
     sin_phi = np.sin( phi_rad )
     denum = np.sqrt(1.0-(2.0*earth_f-earth_f**2)*sin_phi**2)
-    r_xy = Re/denum+elev
-    r_z = Re*((1.0-earth_f)**2)/denum+elev
+    r_xy = Re/denum+elev/1000.0
+    r_z = Re*((1.0-earth_f)**2)/denum+elev/1000.0
 
     # compute cartesian components of geocentric (non-rotating) observer position
     x_gc = r_xy*cos_phi_cos_theta

orbitdeterminator/kep_determination/radec_LS_test_satobs.py
@@ -102,17 +102,19 @@ def t_radec_res_vec(x, inds, iod_object_data, sat_observatories_data, rov):
 
 # path of file of optical IOD-formatted observations
 # the example contains tracking data for satellite USA 74
+# body_fname_str = '../example_data/iss_radec_generated_horizons.txt'
 body_fname_str = '../example_data/SATOBS-ML-19200716.txt'
 
 #body name
 body_name_str = 'ISS (25544)'
 
 #lines of observations file to be used for orbit determination
-obs_arr = [1, 4, 6] # ML observations of ISS on 2016 Jul 19 and 20
+# obs_arr = [7, 10, 13] # simulated observations generated from HORIZONS for ISS on 2018 Aug 08
+obs_arr = [2, 3, 4, 5] # ML observations of ISS on 2016 Jul 19 and 20
 
 # load IOD data for a given satellite
 iod_object_data = gm.load_iod_data(body_fname_str)
-# print('IOD observation data:\n', iod_object_data[ np.array(obs_arr)-1 ], '\n')
+print('IOD observation data:\n', iod_object_data[ np.array(obs_arr)-1 ], '\n')
 
 #load data of listed observatories (longitude, latitude, elevation)
 sat_observatories_data = gm.load_sat_observatories_data('sat_tracking_observatories.txt')
@@ -141,6 +143,7 @@ x0 = q0[0:6]
 x0[3:6] = np.deg2rad(x0[3:6])
 # print('x0 = ', x0)
 
+# obs_arr_ls = np.array(range(1, 31+1))
 obs_arr_ls = np.array(range(1, 6+1))
 print('obs_arr_ls = ', obs_arr_ls)
 # print('obs_arr_ls[0] = ', obs_arr_ls[0])