ef0367411318b4b4b5de0e610349be1a27557a7a
Vidhan <vidhanarya50@gmail.com>
2019-06-18 04:39:29 +0530

    Fix failing test case

--- orbitdeterminator/tests/test_get_format.py
+++ orbitdeterminator/tests/test_get_format.py
@@ -1,11 +1,13 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 from util import get_format as gf
 import pytest
 
+TEST_FILE = os.path.dirname(os.path.realpath(__file__)) + "/../example_data/Test/"
+
 def test_get_format():
-	assert gf.get_format("example_data/Test/test_uk.txt") == "UK"
-	assert gf.get_format("example_data/Test/test_rde.txt") == "RDE"
-	assert gf.get_format("example_data/Test/test_iod.txt") == "IOD"
+	assert gf.get_format(TEST_FILE + "test_uk.txt") == "UK"
+	assert gf.get_format(TEST_FILE + "test_rde.txt") == "RDE"
+	assert gf.get_format(TEST_FILE + "test_iod.txt") == "IOD"
