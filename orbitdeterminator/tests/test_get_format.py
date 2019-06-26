82e1c00daf4c709ac311e5498d3f7e6b2f05cb68
Vidhan <vidhanarya50@gmail.com>
2019-06-26 07:55:23 +0530

    Adds user option to input multiple files in multiple formats

--- orbitdeterminator/tests/test_get_format.py
+++ orbitdeterminator/tests/test_get_format.py
@@ -5,9 +5,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 from util import get_format as gf
 import pytest
 
-TEST_FILE = os.path.dirname(os.path.realpath(__file__)) + "/../example_data/Test/"
+TEST_FILE = os.path.dirname(os.path.realpath(__file__)) + "/../example_data/SourceTXT/"
 
 def test_get_format():
-	assert gf.get_format(TEST_FILE + "test_uk.txt") == "UK"
-	assert gf.get_format(TEST_FILE + "test_rde.txt") == "RDE"
-	assert gf.get_format(TEST_FILE + "test_iod.txt") == "IOD"
+	assert gf.get_format(TEST_FILE + "uk_sample.txt") == "UK"
+	assert gf.get_format(TEST_FILE + "rde_sample.txt") == "RDE"
+	assert gf.get_format(TEST_FILE + "iod_sample.txt") == "IOD"
+	assert gf.get_format(os.path.dirname(os.path.realpath(__file__)) + "/../example_data/SourceCSV/orbit.csv") == "Cartesian"
