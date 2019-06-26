aerospaceresearch/orbitdeterminator

commit 3f15a58c8da4e65b7564439f405e85244e904d1e

Adds user option to input multiple files in multiple formats

orbitdeterminator/util/convert_format.py
@@ -9,7 +9,7 @@ import astropy.time as at
 '''
 Assumes that all input files are present in $PROJ_DIR$/orbitdeterminator/example_data/Source_CSV/
 '''
-SOURCE_ABSOLUTE = os.getcwd() + "/example_data"  # Absolute path of source directory
+SOURCE_ABSOLUTE = os.getcwd() + "/../example_data"  # Absolute path of source directory
 
 '''
 A dictionary that contains details of all observation sites
@@ -814,6 +814,67 @@ def read_iod_uk_file(path):
 	myfile.close()
 	return(iod_uk_file)
 
+def geographic_to_ecef(geographic):
+	'''
+	Converts the given Geographic coordinates to ECEG (X, Y and Z) coordinates
+
+	Args:
+		geographic observation (numoy array): Array of data points in geographic coordinates (Latitude (degrees), Longitute (degrees) and Altitude (meters)) 
+
+	Returns:
+		observation (numpy array): Array of data points in ECEF (X, Y and Z (in meters)) coordinates
+	'''
+	cosLat = np.cos(geographic[:, 0] * np.PI / 180.0)
+	sinLat = np.sin(geographic[:, 0] * np.PI / 180.0)
+	cosLon = np.cos(geographic[:, 1] * np.PI / 180.0)
+	sinLon = np.sin(geographic[:, 1] * np.PI / 180.0)
+	radius = 6378137.0
+	f = 1.0 / 298.257224
+	C = 1.0 / np.sqrt(cosLat * cosLat + (1 - f) * (1 - f) * sinLat * sinLat)
+	S = (1.0 - f) * (1.0 - f) * C
+	h = 0.0
+	x = (radius * C + geographic[:, 2]) * cosLat * cosLon
+	y = (radius * C + geographic[:, 2]) * cosLat * sinLon
+	z = (radius * S + geographic[:, 2]) * sinLat
+	ECEF = np.concatenate((x, y, z), axis=1)
+	return ECEF
+
+def convert_format(file, file_format):
+	'''
+	Converts the given observation format into cartesian coordinate system
+
+	Args:
+		path (string): Path to a file in example_data folder
+		observation format (string): Observation format of file (I.O.D., R.D.E. or U.K.)
+
+	Returns:
+		observation (numpy array): A array of floats containing observation in [t, x, y, z] format
+	'''
+	data = read_iod_uk_file(SOURCE_ABSOLUTE + "/Source" + file[-3:].upper() + "/{}".format(file))
+	data_parsed = np.empty((0, 4))
+	for line in data:
+		if file_format == "UK":
+			uk_data = parse_uk(line)
+			position_deg = pos_coord_to_str(uk_data[5])
+			R = range_to_str_uk(uk_data[6])
+			latitude = float(re.split(" ", position_deg)[2 if len(re.split(" ", position_deg)) > 2 else 1])
+			longitude = float(re.split(" ", position_deg)[0])
+			radius = float(6378137.0 if R == 'NA' else R)
+			timestamp = datetime.datetime.strptime(datetime_to_str(uk_data[2]), "%Y-%m-%d %H:%M:%S.%f").timestamp()
+		elif file_format == "IOD":
+			iod_data = parse_iod(line)
+			position_deg = pos_coord_to_str(iod_data[6])
+			latitude = float(re.split(" ", position_deg_str)[2 if len(re.split(" ", position_deg_str)) > 2 else 1])
+			longitude = float(re.split(" ", position_deg)[0])
+			radius = float(6378137.0)
+			timestamp = datetime.datetime.strptime(datetime_to_str(iod_data[4]), "%Y-%m-%d %H:%M:%S.%f").timestamp()
+		data_parsed = np.append(data_parsed, np.array([timestamp, latitude, longitude, radius]))
+	data_parsed[:, 1:] = geographic_to_ecef(data_parsed[:, 1:])
+	return data_parsed
+
+
+
+
 if(__name__ == "__main__"):
 	# Open the GeoJSON file containing observation sites details into a dictionary
 	json_file = open(SOURCE_ABSOLUTE + "/Sites.gjson")
@@ -824,15 +885,3 @@ if(__name__ == "__main__"):
 
 	uk_file = read_iod_uk_file(SOURCE_ABSOLUTE + "/SourceTXT/uk_sample.txt")
 	print_uk(uk_file)
-
-	# rde_file = []
-	# rde_file.append("2420 0309 0.211 1204")
-	# rde_file.append("03")
-	# rde_file.append("0300203 195141.71 005932+313611 4.4 4.4 0 S")
-	# rde_file.append("8502002 195853.74 010744+311633 5.1 5.1 0 S")
-	# rde_file.append("04")
-	# rde_file.append("7704401 015519.00 212317+765245 6.3 7.5 4 R")
-	# rde_file.append("7704401 015559.35 221342+565110 6.0 7.2 4 R")
-	# rde_file.append("8700602 020411.32 151747+741235 4.8 4.8 0 S")
-	# rde_file.append("8205102 021426.21 021503+282843 4.2 4.2 0 S")
-	# rde_file.append("999")

orbitdeterminator/main.py
@@ -3,8 +3,8 @@ Runs the whole process in one file for a .csv positional data file (time, x, y, 
 and generates the final set of keplerian elements along with a plot and a filtered.csv data file
 '''
 
-
-from util import (read_data, kep_state, rkf78, golay_window)
+import warnings
+from util import (read_data, kep_state, rkf78, golay_window, get_format, convert_format)
 from filters import (sav_golay, triple_moving_average)
 from kep_determination import (lamberts_kalman, interpolation, ellipse_fit, gibbsMethod)
 import automated
@@ -15,26 +15,37 @@ import matplotlib.pylab as plt
 from propagation import sgp4
 import inquirer
 
-def process(data_file, error_apriori, units):
+def process(data_file, error_apriori, units, output):
     '''
     Given a .csv data file in the format of (time, x, y, z) applies both filters, generates a filtered.csv data
     file, prints out the final keplerian elements computed from both Lamberts and Interpolation and finally plots
     the initial, filtered data set and the final orbit.
 
     Args:
-        data_file (string): The name of the .csv file containing the positional data
+        data_file (string): The name of the file containing the positional data
         error_apriori (float): apriori estimation of the measurements error in km
 
     Returns:
         Runs the whole process of the program
     '''
-    # First read the csv file called "orbit" with the positional data
-    source_path = "example_data/SourceCSV/"
+    source_path = "example_data/Source"
     dest_path = "example_data/DestinationCSV/"
     data_file = data_file.split(" ")
-    data = read_data.load_data(source_path + data_file[0])
-    for file in data_file[1: ]:
-        data = numpy.concatenate((data, read_data.load_data(source_path + file)), axis=0)
+    data = np.empty((0, 4))
+    for file in data_file:
+        try:
+            file_format = get_format.get_format(source_path + file[-3:].upper() + '/{}'.format(file))
+        except:
+            warnings.warn("Skiping {} file (Not a valid file type)".format(file))
+            continue
+
+        if file_format in ['IOD', 'RDE', 'UK']:
+            data = np.append(data, convert_format.convert(file, file_format), axis=0)
+        elif file_format is 'Cartesian':
+            data = np.append(data, read_data.load_data(source_path + file[-3:].upper() + "/{}".format(file)), axis=0)
+        else:
+            print("Skiping {} file (Observation format is undefined)".format(file))
+
 
 
     print("***********Choose from following options in desired order of application***********")
@@ -97,7 +108,7 @@ def process(data_file, error_apriori, units):
     print(means, "\n")
 
     # Save the filtered data into a new csv called "filtered"
-    np.savetxt(dest_path + data_file.strip(".csv") + "_filtered.csv", data_after_filter, delimiter=",")
+    np.savetxt(dest_path + output + ".csv", data_after_filter, delimiter=",")
 
     kep_elements = {}
 
@@ -235,6 +246,7 @@ def read_args():
     parser.add_argument('-f', '--file_path', type=str, help="Path to .csv data file", default='orbit.csv')
     parser.add_argument('-e', '--error', type=float, help="Estimation of the measurement error", default=10.0)
     parser.add_argument('-u', '--units', type=str, help="m for metres, k for kilometres", default='k')
+    parser.add_argument('-o', '--output', type=str, help="Filename to save filtered data in DestinationCSV folder", default='filtered')
     parser.add_argument('-a', '--automate', help="Automate the orbit determination process", action='store_true')
     return parser.parse_args()
 
@@ -256,4 +268,4 @@ if __name__ == "__main__":
     if args.automate:
         automated.main()
     else:
-        process(args.file_path, args.error, args.units)
+        process(args.file_path, args.error, args.units, args.output)

orbitdeterminator/tests/test_get_format.py
@@ -5,9 +5,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 from util import get_format as gf
 import pytest
 
-TEST_FILE = os.path.dirname(os.path.realpath(__file__)) + "/../example_data/Test/"
+TEST_FILE = os.path.dirname(os.path.realpath(__file__)) + "/../example_data/SourceTXT/"
 
 def test_get_format():
-	assert gf.get_format(TEST_FILE + "test_uk.txt") == "UK"
-	assert gf.get_format(TEST_FILE + "test_rde.txt") == "RDE"
-	assert gf.get_format(TEST_FILE + "test_iod.txt") == "IOD"
+	assert gf.get_format(TEST_FILE + "uk_sample.txt") == "UK"
+	assert gf.get_format(TEST_FILE + "rde_sample.txt") == "RDE"
+	assert gf.get_format(TEST_FILE + "iod_sample.txt") == "IOD"
+	assert gf.get_format(os.path.dirname(os.path.realpath(__file__)) + "/../example_data/SourceCSV/orbit.csv") == "Cartesian"

orbitdeterminator/util/get_format.py
@@ -27,18 +27,28 @@ def get_format(data_file):
 	Detects the format of given file
 
 	Args:
-		date_file (string): Location of .txt file inside the orbitdeterminator/example_data folder
+		date_file (string): Location of the file inside the orbitdeterminator/example_data folder
 
 	Returns:
 		String :-
+			'Cartesian': If observation is in Cartesian coordinate system
 			'RDE': If observation is in R.D.E. format
 			'IOD': If observation is in I.O.D. format
 			'UK': If observation is in U.K. format
 			'Undefined format': If observation format is not among R.D.E., I.O.D. or U.K. format
+			Error message if data file extension is neither .txt nor .csv
 	'''
-	lines = [line.rstrip('\n') for line in open(data_file)]
+	Error = "Please use data in '.txt' or '.csv' file formats"
+	if data_file[-4:] == ".txt":
+		lines = [line.rstrip('\n') for line in open(data_file)]
+	elif data_file[-4:] == ".csv":
+		lines = np.genfromtxt(data_file, delimiter='\t')
+	else:
+		return Error
 	length = [(len(line)) for line in lines]
-	if length[0] == 20:
+	if length[0] == 4:
+		return 'Cartesian'
+	elif length[0] == 20:
 		return 'RDE'
 	elif length[0] <= 80:
 		if (checkdate(lines[0][23:31])):
@@ -47,8 +57,10 @@ def get_format(data_file):
 			return 'UK'
 		else:
 			return 'Undefined format'
+	else:
+		return "Undefined format"
 
 if __name__ == "__main__":
 	
-	data_file = "../example_data/test.txt"
-	get_format(data_file)
+	data_file = "../example_data/SourceTXT/uk_sample.txt"
+	print(get_format(data_file))

commit c600bd62f8ac09d76700af8387df096763b0bf65

Fix import error caused in commit 3f15a58

orbitdeterminator/util/convert_format.py
@@ -872,9 +872,6 @@ def convert_format(file, file_format):
 	data_parsed[:, 1:] = geographic_to_ecef(data_parsed[:, 1:])
 	return data_parsed
 
-
-
-
 if(__name__ == "__main__"):
 	# Open the GeoJSON file containing observation sites details into a dictionary
 	json_file = open(SOURCE_ABSOLUTE + "/Sites.gjson")