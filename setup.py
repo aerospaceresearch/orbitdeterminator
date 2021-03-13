bc2df49badd517a3d997abb78081768b9acac386
hornig <chefkueche@hornshouse.com>
2021-03-13 23:38:06 +0100

    adds requirements install and adds python 3.8
    adds requirements install
    adds python 3.8, because travis did not build in 3.5 anymore.
    adds pygmo to the requirements.txt

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from setuptools import *
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
 setup(
 	name="orbitdeterminator",
 	packages=find_packages(),
 	version="1.0",
 	license="MIT",
-	author="Alexandros Kazantzidis, Nilesh Chaturvedi",
-	author_email="alexandroskaza23@gmail.com, n.chaturvedi3@gmail.com",
+	author="aerospaceresearch.net community",
+	author_email="orbitdeterminator@aerospaceresearch.net",
 	url="http://orbit-determinator.readthedocs.io/en/latest/",
 	description="__orbitdeterminator__ is a package written in python3 for determining orbit of satellites based " \
 				  "on positional data. Various filtering and determination algorithms are available for satellite " \
 				  "operators to choose from.  ",
-	install_requires=[
-		"numpy",
-		"scipy",
-		"matplotlib",
-		"pykep",
-		"pytest",
-		"inquirer",
-	]
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+	install_requires=[req for req in requirements if req[:2] != "# "],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
 )
